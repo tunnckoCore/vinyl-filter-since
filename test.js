0f12a902a38b8b31dfecb64148b9ad50b0fb3583
tunnckoCore <mameto_100@mail.bg>
2015-03-31 05:34:47 +0300

    update tests, 100% coverage

--- test.js
+++ test.js
@@ -7,50 +7,57 @@
 
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var gulp = require('gulp');
 var assert = require('assert');
 var vinylFilterSince = require('./index');
 var through2 = require('through2');
 
+var fixture = path.join(__dirname, './package.json');
+
+// testing framework
+var lab = exports.lab = require('lab').script();
+var describe = lab.describe;
+var it = lab.it;
+
+// DRY
+function testit(lastUpdateDate, len, done) {
+  var stream = gulp.src('*.json');
+  var files = [];
+
+  stream
+    .pipe(through2.obj(function(file, enc, next) {
+      next(null, file);
+    }))
+    .pipe(vinylFilterSince(lastUpdateDate))
+    .pipe(through2.obj(function(file, enc, next) {
+      files.push(file);
+      next();
+    }));
+
+  stream.once('end', function _end() {
+    assert.strictEqual(files.length, len);
+    done();
+  });
+}
+
 describe('vinyl-filter-since:', function() {
-  it('should work properly', function(done) {
-    gulp.src('test.js')
-      .pipe(vinylFilterSince(new Date('2000-03-24 10:30:00')))
-      .pipe(through2.obj(function(file, enc, next) {
-        done();
-      }))
+  it('should throw TypeError if no Date or Number given', function(done) {
+    assert.throws(function _fixture() {
+      testit('foo bar', 0, function() {});
+    }, /to be date or number/);
+
+    assert.throws(function _fixture() {
+      testit([1, 2, 3], 0, function() {});
+    }, TypeError);
+
+    done();
   });
-  it('should not work properly when time is in future (with debug `true`)', function(done) {
-    gulp.src('test.js')
-      .pipe(vinylFilterSince(new Date('2020-03-24 10:30:00'), true))
-      .pipe(through2.obj(function(file, enc, next) {
-        // file.debug is set internally when second argument
-        // is given and it is `true`
-        // it is only for testing purposes
-        if (file.debug) {
-          next(null, true);
-          return;
-        }
-        next(null, file);
-      }))
-      .pipe(through2.obj(function(isOkey, enc, next) {
-        assert.strictEqual(isOkey, true);
-        done();
-      }));
+  it('should glob a file changed after since', function(done) {
+    testit(new Date((+fs.statSync(fixture).mtime) - 2200), 1, done);
   });
-  it('should work properly when time is in past (with debug `true`)', function(done) {
-    gulp.src('test.js')
-      .pipe(through2.obj(function(file, enc, next) {
-        file.work = true;
-        next(null, file);
-      }))
-      .pipe(vinylFilterSince(new Date('2000-03-24 16:12:00'), true))
-      .pipe(through2.obj(function(file, enc, next) {
-        assert.strictEqual(file.work, true);
-        assert.strictEqual(file.debug, undefined);
-        // file.debug is undefined, because it is defined
-        // after the check for `filterSince` so not reached.
-        done();
-      }));
+  it('should not glob a file changed before since', function(done) {
+    testit(new Date((+fs.statSync(fixture).mtime) + 1300), 0, done);
   });
 });
